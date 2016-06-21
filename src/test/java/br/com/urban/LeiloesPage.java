cd4743a87741cd1cf364553cfb189360a5985d47
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-06-21 12:36:45 -0300

    Update LeiloesPage.java

--- src/test/java/br/com/urban/LeiloesPage.java
+++ src/test/java/br/com/urban/LeiloesPage.java
@@ -1,7 +1,10 @@
 package br.com.urban;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LeiloesPage {
 
@@ -16,12 +19,19 @@ public class LeiloesPage {
 	}
 	
 	public NovoLeilaoPage novo() {
-		driver.findElement(By.linkText("Novo Leil„o")).click();
+		driver.findElement(By.linkText("Novo Leil√£o")).click();
 		return new NovoLeilaoPage(driver);
 	}
 	
 	public boolean existe(String produto, double valor, String usuario, boolean usado) {
 		return driver.getPageSource().contains(produto) && driver.getPageSource().contains(String.valueOf(valor))
-				&& driver.getPageSource().contains(usado ? "Sim" : "N„o");
+				&& driver.getPageSource().contains(usado ? "Sim" : "N√£o");
 	}
+	
+	public DetalhesDoLeilaoPage detalhes(int posicao) {
+        List<WebElement> elementos = driver.findElements(By.linkText("exibir"));
+        elementos.get(posicao - 1).click();
+
+        return new DetalhesDoLeilaoPage(driver);
+    }
 }
